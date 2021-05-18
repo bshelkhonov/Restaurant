4e4e56befe6173e68e0d66a042dd04d205b5a647
bshelkhonov <shelbulat@mail.ru>
2021-05-18 23:45:05 +0300

    fixed types

--- sql/create.sql
+++ sql/create.sql
@@ -4,7 +4,7 @@ set search_path to restaurant, public;
 
 create table "employee"
 (
-    employee_id bigint not null primary key,
+    employee_id integer not null primary key,
     first_nm    varchar(256),
     last_nm     varchar(256),
     salary      integer,
@@ -14,7 +14,7 @@ create table "employee"
 create table if not exists "administrator"
 (
     administrator_id serial primary key not null,
-    employee_id      bigint             not null references "employee" (employee_id)
+    employee_id      integer            not null references "employee" (employee_id)
 );
 
 create table if not exists "hall"
@@ -26,15 +26,15 @@ create table if not exists "hall"
 
 create table if not exists "hall_x_administrator"
 (
-    hall_id          bigint not null references "hall" (hall_id),
-    administrator_id bigint not null references "administrator" (administrator_id),
+    hall_id          integer not null references "hall" (hall_id),
+    administrator_id integer not null references "administrator" (administrator_id),
     constraint hall_administrator_id primary key (hall_id, administrator_id)
 );
 
 create table if not exists "cook"
 (
     cook_id     serial primary key not null,
-    employee_id bigint             not null references "employee" (employee_id)
+    employee_id integer            not null references "employee" (employee_id)
 );
 
 create table if not exists "dish"
@@ -45,8 +45,8 @@ create table if not exists "dish"
 
 create table if not exists "cook_x_dish"
 (
-    cook_id bigint references "cook" (cook_id),
-    dish_id bigint references "dish" (dish_id),
+    cook_id integer not null references "cook" (cook_id),
+    dish_id integer not null references "dish" (dish_id),
     constraint dish_cook_id primary key (cook_id, dish_id)
 );
 
@@ -59,29 +59,29 @@ create table if not exists "product"
 
 create table if not exists "product_x_dish"
 (
-    product_id bigint references "product" (product_id),
-    dish_id    bigint references "dish" (dish_id),
+    product_id integer not null references "product" (product_id),
+    dish_id    integer not null references "dish" (dish_id),
     constraint product_dish_id primary key (product_id, dish_id)
 );
 
 create table if not exists "customer"
 (
-    customer_id    bigint primary key not null,
+    customer_id    integer primary key not null,
     first_nm       varchar(256),
     last_nm        varchar(256),
-    customer_since timestamp          not null default now()
+    customer_since timestamp           not null default now()
 );
 
 create table if not exists "card"
 (
-    card_id bigint primary key not null,
-    dish_id bigint             not null references "dish" (dish_id),
-    bonuses integer            not null default 0
+    card_id integer primary key not null,
+    dish_id integer references "dish" (dish_id),
+    bonuses integer             not null default 0
 );
 
 create table if not exists "customer_x_card"
 (
-    customer_id bigint references customer (customer_id),
-    card_id     bigint not null references "card" (card_id),
+    customer_id integer not null references customer (customer_id),
+    card_id     integer not null references "card" (card_id),
     constraint customer_x_card_id primary key (customer_id, card_id)
 );
